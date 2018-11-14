1abcc67dd33b70123687fe1cf6428e8c673d10ab
Martin Cox <martin.cox@aad.gov.au>
2018-11-14 15:10:01 +0000

    added a function to get calibration values EVgetCalParVlaues() and a function to check an Echoview acoustic virtual acoustiEVAcoVarCheck()

---
+++ R/EVgetCalParValues.R
@@ -0,0 +1,64 @@
+#' Get calibration settings pf an Echoview acoustic variable using COM
+#' 
+#' This function gets the calibration values of an Echoview acousic variable using COM. 
+#' @details The function can read three types of calibration values "default", "set" and "used".
+#''@param EVFile ("COMIDispatch) An Echoview file COM object 
+#' @param acoVarName (character) Name of Echoview single target virtual variable
+#' @param pingNumber = 0 (numeric) Ping number from which to extract calibration values.
+#' @param type = =c('default','set','used') (character) Calibration value type.
+#' @param verbose = FALSE (boolean) Should the calibration values be displayed in the console.
+#' @references \url{http://support.echoview.com/WebHelp/Echoview.htm/}
+#' @seealso \code{\link{EVAcoVarNameFinder}}
+#' @export
+EVgetCalParValues=function(EVFile,acoVarName,pingNumber=1,type=c('default','set','used'),verbose=FALSE){
+  #set-up output
+  msgV=paste(Sys.time(),' : looking for calibration values for acoustic variable', acoVarName)
+  message(msgV)
+  
+  out=list(obj=NULL,
+    Name=acoVarName,
+    pingNumber=NULL,
+    type=NULL,
+    pars=NULL,
+    msg=msgV)
+  
+  EVVar=EVAcoVarNameFinder(EVFile=EVFile, acoVarName=acoVarName)
+  EVVar=EVVar$EVVar
+  msgV=c(msgV,EVVar$msg)
+  out$msg=msgV
+  if(is.null(EVVar)) {
+    msg=paste(Sys.time(),' : Acoustic variable', acoVarName ,'not found. Exiting EVgetParValues()')
+    warning(msg)
+    msgV=c(msgV,msg)
+    out$msg=msgV
+    return(out)
+  }
+  out$obj=EVVar
+  out$pingNumber=pingNumber
+  out$type=type
+  
+  cal=EVVar[['Properties']][['Calibration']]
+  print('here!')
+  if(type=='set') calVarName=strsplit(cal$GetAllSet(pingNumber),' ')[[1]]
+  if(type=='used') calVarName=strsplit(cal$GetAllUsed(pingNumber),' ')[[1]]
+  if(type=='default') calVarName=strsplit(cal$GetDefault(pingNumber),' ')[[1]]
+  if(length(calVarName)==0)
+  {
+    msg=paste(Sys.time(),': no calibration parameters of the',type,'type were found in',acoVarName)
+    warning(msg)
+    msgV=c(msgV,msg)
+    out$msg=msgV
+    return(out)
+  }
+  print(calVarName)
+  out<<-calVarName
+  parVSet=vector(mode='numeric',length=length(calVarName))
+  for(i in 1:length(calVarName)) parVSet[i]=cal$Get(calVarName[i],pingNumber)
+  out$pars=data.frame(parName=calVarName,parval=parVSet)
+  if(verbose){
+  for(i in 1:length(calVarName))
+    message(paste(calVarName[i],parVSet[i],sep=' = '))}
+  
+  return(out)
+}
+
--- R/EchoviewR.R
+++ R/EchoviewR.R
@@ -539,40 +539,6 @@ EVSchoolsDetSet  <- function (EVFile, varObj, distanceMode,
 }
 
 
-#' Find an acoustic variable by name
-#' 
-#' This function finds an acoustic variable in an Echoview file by name and 
-#' returns the variable pointer.
-#' @param EVFile An Echoview file COM object
-#' @param acoVarName The name of an acoustic variable in the Echoview file
-#' @return a list object with two elements. $EVVar: An Echoview acoustic variable object, and $msg: message for processing log.
-#' @keywords Echoview COM scripting
-#' @export
-#' @references \url{http://support.echoview.com/WebHelp/Echoview.htm/}
-#' @seealso \code{\link{EVOpenFile}}
-#' @examples
-#' \dontrun{
-#' EVAppObj <- COMCreate('EchoviewCom.EvApplication')
-#' EVFile <- EVOpenFile(EVAppObj,'~~/KAOS/KAOStemplate.EV')$EVFile
-#' varObj <- EVAcoVarNameFinder(EVFile, "120 7x7 convolution")$EVVar
-#' }
-EVAcoVarNameFinder <- function (EVFile, acoVarName) {
-  
-  obj <- EVFile[["Variables"]]$FindByName(acoVarName)
-  if (is.null(obj)) {
-    obj <- EVFile[["Variables"]]$FindByShortName(acoVarName)
-  }
-  if (is.null(obj)) {
-    msg <- paste(Sys.time(), ' : Variable not found ', acoVarName, sep = '')
-    stop(msg)
-    obj <- NULL
-  } else {
-    msg <- paste(Sys.time(), ' : Variable found ', acoVarName, sep = '') 
-    message(msg)
-  }
-  return(list(EVVar = obj, msg = msg))  
-}
-
 #' Find an Echoview region class by name
 #' 
 #' This function finds an echoview region class by name.
